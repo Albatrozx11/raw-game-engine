ef85be32b23de18485c8ec509ab238be743da4b9
AdithyanA <adithyanayithala@gmail.com>
2024-07-21 13:13:47 +0530

    initialized cube mesh

--- main.cpp
+++ main.cpp
@@ -14,10 +14,61 @@ struct mesh {
 
 
 int main() {
-	sf::RenderWindow window(sf::VideoMode(256, 240), "RAW");
+	sf::ContextSettings settings;
+	settings.antialiasingLevel = 8;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "RAW",sf::Style::Default,settings);
 
 	while (window.isOpen()) {
 		sf::Event event;
+
+		while (window.pollEvent(event)) {
+
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.setVerticalSyncEnabled(true);
+
+		mesh meshCube;
+
+		meshCube.tris = {
+			//SOUTH
+			{0.0f, 0.0f, 0.0f,  0.0f, 1.0f, 0.0f,  1.0f, 1.0f, 0.0f,},
+			{0.0f, 0.0f, 0.0f,  1.0f, 1.0f, 0.0f,  1.0f, 0.0f, 0.0f,},
+
+
+			//EAST
+			{1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, },
+			{1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, },
+
+
+			//NORTH
+			{1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f, 1.0f, },
+			{1.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 0.0f, 1.0f, },
+
+
+			//WEST
+			{0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 0.0f, 1.0f, 0.0f, },
+			{0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, },
+
+
+			//TOP
+			{0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, },
+			{0.0f, 1.0f, 0.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 0.0f, },
+
+			//BOTTOM
+			{1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, },
+			{1.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, },
+
+
+		};
+
+		window.clear(sf::Color::Black);
+		sf::CircleShape circle(100.0f);
+
+		circle.setFillColor(sf::Color::Red);
+		window.draw(circle);
+		window.display();
 
 	}
 	return 0;
